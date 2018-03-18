fede8f2cc9fb5d6420a6bb01e706cfa74cd28a17
Loes <s1101632@student.hsleiden.nl>
2018-03-18 23:29:59 +0100

    added search bar

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ window.onload = function()
 {
 	var script = document.createElement('script');
 	
-	script.src = 'https://maps.googleapis.com/maps/api/js?key=' + apiKey + '&callback=initMap';
+	script.src = 'https://maps.googleapis.com/maps/api/js?key=' + apiKey + '&libraries=places&callback=initMap';
 	
 	document.getElementsByTagName('head')[0].appendChild(script);
 }
 
 function initMap(){
+
+	//Oefenen locaties met infowindow
 	var locations = [
 		['Leiden', 52.1674, 4.4713, 4],
 		['The Hague', 52.0705, 4.3007, 5],
@@ -16,42 +18,84 @@ function initMap(){
 		['Maastricht', 50.8514, 5.6910, 1]
 	  ];
   
-	  var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 7,
-		center: new google.maps.LatLng(52.1326, 5.2913),
-		mapTypeId: google.maps.MapTypeId.SATELLITE
-	  });
-  
-	  var infowindow = new google.maps.InfoWindow();
-  
-	  var marker, i;
-	  
-	  var image = {
-			url: '../ikgeo/res/wm.png',
+	var map = new google.maps.Map(document.getElementById('map'), {
+	zoom: 7,
+	center: new google.maps.LatLng(52.1326, 5.2913),
+	mapTypeId: google.maps.MapTypeId.SATELLITE
+	});
 
-		};
+	var infowindow = new google.maps.InfoWindow();
 		
-		var windmolenLayer = new google.maps.KmlLayer({
-			url: "https://loeeess.github.io/ikgeo/data/windmolens.kmz", 
-				map: map
-				
-			});
+	//Laag die de windmolens in Nederland toont
+	var windmolenLayer = new google.maps.KmlLayer({
+		url: "https://loeeess.github.io/ikgeo/data/windmolens.kmz", 
+		map: map
+	});
 		
-	  for (i = 0; i < locations.length; i++) { 
+	//Oefenen met markers in een loop
+	var marker, i;
 
+	for (i = 0; i < locations.length; i++) { 
 		marker = new google.maps.Marker({
-		  position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-		  map: map
+			position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+			map: map
 		});
-  
+		
 		google.maps.event.addListener(marker, 'click', (function(marker, i) {
-		  return function() {
+			return function() {
 			infowindow.setContent(locations[i][0]);
 			infowindow.open(map, marker);
-		  }
+			}
 		})(marker, i));
-		
-		}
-		
-		
+	}
+
+	//zoekbalk
+	var input = document.getElementById('pac-input');
+      var searchBox = new google.maps.places.SearchBox(input);
+      map.controls[google.maps.ControlPosition.TOP_RIGHT].push(input);
+
+	map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+	var markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+  	var places = searchBox.getPlaces();
+      if (places.length == 0) {
+        return;
+      }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+      return;
+    	}
+           
+    // Create a marker for each place.
+    markers.push(new google.maps.Marker({
+      map: map,
+      title: place.name,
+      position: place.geometry.location
+      }));
+
+    if (place.geometry.viewport) {
+    // Only geocodes have viewport.
+      bounds.union(place.geometry.viewport);
+    	} else {
+      bounds.extend(place.geometry.location);
+			}
+		});
+	map.fitBounds(bounds);
+	});
+    		
 }
